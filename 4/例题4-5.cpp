2de6b028a3e0648ec061331cdde783b831da633d
xshahq <15858265922@163.com>
2018-08-11 13:23:44 +0800

    4-5

--- 4/例题4-5.cpp
+++ 4/例题4-5.cpp
@@ -1,93 +1,152 @@
-#include<iostream>
+#include<algorithm>
 #include<cstdio>
-#include<string>
 #include<cstring>
+#include<iostream>
+#include<list>
+#include<vector>
 using namespace std;
- 
-const int MAX = 10000;
-int r,c,n;
-struct cmd
+int r,c,n,or1,oc1,m;
+vector<int> oper;
+vector<int> order;
+void exchange(int& i,int& r1,int& c1)
 {
-    string s;
-    int A,a[20];
-    int r1,c1,r2,c2;
-}cmds[MAX];
- 
-void save(string str, int i)
-{
-    int A;
-    if(str!="EX"){
-        cin>>A;
-        cmds[i].s = str; cmds[i].A = A;
-        for(int j=0;j<A;j++) cin>>cmds[i].a[j];
-    }
-    else if(str=="EX"){
-        cmds[i].s = str;
-        cin>>cmds[i].r1>>cmds[i].c1>>cmds[i].r2>>cmds[i].c2;
-    }
-}
- 
-bool solve(int& x, int& y)
-{
-    int xx,yy;
-    for(int i=0;i<n;i++)
+    if(r1 == oper[i] && c1 == oper[i + 1])
     {
-        if(cmds[i].s=="DR"){
-            xx = x; yy = y;
-            for(int j=0;j<cmds[i].A;j++){
-                if(cmds[i].a[j]<xx) { x--; }
-                else if(cmds[i].a[j]==xx) { return false;}
-            }
-        }
-        else if(cmds[i].s=="DC"){
-            xx = x; yy = y;
-            for(int j=0;j<cmds[i].A;j++){
-                if(cmds[i].a[j]<yy)  {  y--;}
-                else if(cmds[i].a[j]==yy) { return false;}
-            }
-        }
-        else if(cmds[i].s=="IC"){
-            xx = x; yy = y;
-            for(int j=0;j<cmds[i].A;j++){
-                if(cmds[i].a[j]<=yy) y++;
-            }
-        }
-        else if(cmds[i].s=="IR"){
-            xx = x; yy = y;
-            for(int j=0;j<cmds[i].A;j++){
-                if(cmds[i].a[j]<=xx)  x++;
-            }
-        }
-        else if(cmds[i].s=="EX"){
-            if(x==cmds[i].r1&&y==cmds[i].c1) { x = cmds[i].r2; y =cmds[i].c2;}
-            else if(x==cmds[i].r2&&y==cmds[i].c2) { x = cmds[i].r1; y =cmds[i].c1;}
-        }
+        r1 = oper[i + 2];
+        c1 = oper[i + 3];
     }
-    return true;
+    else if(r1 == oper[i + 2] && c1 == oper[i + 3])
+    {
+        r1 = oper[i];
+        c1 = oper[i + 1];
+    }
+    i += 4;
 }
- 
+int deler(int& i,int& r1,int& c1)
+{
+    int temp = oper[i++],temp1 = r1;
+    for(int j = 0;j < temp;++j)
+    {
+        if(temp1 == oper[i])
+            return 0;
+        if(temp1 > oper[i])
+            r1--;
+        ++i;
+    }
+    return 1;
+}
+int delec(int& i,int& r1,int& c1)
+{
+    int temp = oper[i++],temp1 = c1;
+    for(int j = 0;j < temp;++j)
+    {
+
+        if(temp1 == oper[i])
+            return 0;
+        if(temp1 > oper[i])
+            c1--;
+        ++i;
+    }
+    return 1;
+}
+void insertr(int& i,int& r1,int& c1)
+{
+    int temp = oper[i++],temp1 = r1;
+    for(int j = 0;j < temp;++j)
+    {
+        if(temp1 >= oper[i])
+            ++r1;
+        ++i;
+    }
+}
+void insertc(int& i,int& r1,int& c1)
+{
+    int temp = oper[i++],temp1 = c1;
+    for(int j = 0;j < temp;++j)
+    {
+        if(temp1 >= oper[i])
+            ++c1;
+        ++i;
+    }
+}
+void querry(int num)
+{
+    while(num)
+    {
+        int r1,c1;
+        cin >> r1 >> c1;
+        or1 = r1;
+        oc1 = c1;
+        int i = 0;
+        for(int j = 0;i < order.size();++i)
+        {
+            int count1 = 1;
+            switch(order[i])
+            {
+                case 0:exchange(j,r1,c1);break;
+                case 1:count1 = deler(j,r1,c1);break;
+                case 2:count1 = delec(j,r1,c1);break;
+                case 3:insertc(j,r1,c1);break;
+                default:insertr(j,r1,c1);
+            }
+            //cout << r1 << c1 <<  endl;
+            if(!count1)
+                break;
+        }
+        i == order.size() ? printf("Cell data in (%d,%d) moved to (%d,%d)\n",or1,oc1,r1,c1) : printf("Cell data in (%d,%d) GONE\n",or1,oc1);
+        --num;
+    }
+}
 int main()
 {
-    int icase = 0;
-    while(cin>>r>>c)
+    int time = 1;
+    while(1)
     {
-        if(r==0&&c==0) break;
-        cin>>n;
-        if(icase>0) cout<<endl;
-        for(int i=0;i<n;i++)
+        cin >> r >> c;
+        if(!r && !c)
+            break;
+        cin >> n;
+        while(n)
         {
-            string str; cin>>str;
-            save(str,i);
+            string str;
+            cin >> str;
+            if(str[0] == 'E')
+            {
+                for(int i = 0;i < 4;++i)
+                {
+                    int temp;
+                    cin >> temp;
+                    oper.push_back(temp);
+                }
+                order.push_back(0);
+            }
+            else
+            {
+                int i = 1,temp;
+                if(str == "DC")
+                    i = 2;
+                else if(str == "IC")
+                    i = 3;
+                else if(str == "IR")
+                    i = 4;
+                cin >> temp;
+                oper.push_back(temp);
+                for(int j = 0;j < temp;++j)
+                {
+                    int temp1;
+                    cin >> temp1;
+                    oper.push_back(temp1);
+                }
+                order.push_back(i);
+            }
+            --n;
         }
-        cout<<"Spreadsheet #"<<++icase<<endl;
-        int q; cin>>q;
-        while(q--)
-        {
-            int x,y,xx,yy;
-            cin>>x>>y; xx = x; yy = y;
-            if(solve(x,y)) printf("Cell data in (%d,%d) moved to (%d,%d)\n",xx,yy,x,y);
-            else printf("Cell data in (%d,%d) GONE\n",xx,yy);
-        }
+        printf("Spreadsheet #%d\n",time++);
+        cin >> m;
+        querry(m);
+        oper.clear();
+        order.clear();
+        cout << endl;
     }
     return 0;
 }
